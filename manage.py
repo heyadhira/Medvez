96d2f7663a21a13b6a422c322754636b40d08ac7
heyadhira <hey.adhira@gmail.com>
2024-09-24 11:20:30 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frontend.settings")
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +15,14 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    
+    # Check if the script is called with 'runserver' command
+    if len(sys.argv) > 1 and sys.argv[1] == "runserver":
+        # Default to port 8000 or use environment variable for PORT
+        port = os.getenv("PORT", "8000")
+        sys.argv.append(f"0.0.0.0:{port}")
 
+    execute_from_command_line(sys.argv)
 
 if __name__ == "__main__":
     main()
